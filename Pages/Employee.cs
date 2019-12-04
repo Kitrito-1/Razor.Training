9befe1863b9c597473d8cdf009ecade0ea26558a
Johnny <0830109@gmail.com>
2019-12-04 16:58:11 +0800

    Update Employee.cs

--- Pages/Employee.cs
+++ Pages/Employee.cs
@@ -9,8 +9,7 @@ namespace Razor.Training
     public class Employee
     {
         [StringLength(10, ErrorMessage = "{0} 長度必須介於2與10字元之間", MinimumLength = 2)]
-        //[Required(ErrorMessage = "{0} 必須輸入")]
-        [Required]
+        [Required(ErrorMessage = "{0} 必須輸入")]
         [Display(Name = "姓名",Prompt = "Ex:Johnny")]
         public String Name { get; set; }
 
